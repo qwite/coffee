7c278f35f74b3832be0df41fc4c05c53e1a8eef2
Artem <qwites@gmail.com>
2022-03-12 21:26:20 +0300

    Initial Commit

---
+++ coffee-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coffee-app
+//
+//  Created by Artem Lashmanov on 3/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

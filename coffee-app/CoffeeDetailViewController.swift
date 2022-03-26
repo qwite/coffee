0b47f363644a96ab1c47f2b404dec81fd1f7a738
Artem <qwites@gmail.com>
2022-03-26 19:35:37 +0300

    added stepper and segmentedControl

--- coffee-app/CoffeeDetailViewController.swift
+++ coffee-app/CoffeeDetailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class CoffeeDetailViewController: UIViewController {
-
+    
     let coffeeImage: UIImageView = {
         let image = UIImageView(image: #imageLiteral(resourceName: "caffelatte"))
         image.contentMode = .scaleAspectFit
@@ -47,10 +47,62 @@ class CoffeeDetailViewController: UIViewController {
         return label
     }()
     
+    let countLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let sizeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Размер:"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let stepper: UIStepper = {
+        let stepper = UIStepper()
+        stepper.stepValue = 1
+        stepper.maximumValue = 5
+        stepper.minimumValue = 1
+        
+        stepper.addTarget(self, action: #selector(stepperValueChanged), for: .valueChanged)
+        stepper.translatesAutoresizingMaskIntoConstraints = false
+        return stepper
+    }()
+    
+    let segmentControl: UISegmentedControl = {
+        let segmentControl = UISegmentedControl(items: ["S", "M", "L"])
+        segmentControl.selectedSegmentIndex = 0
+        segmentControl.translatesAutoresizingMaskIntoConstraints = false
+        return segmentControl
+    }()
+    
+    let addToOrderButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("Оформить заказ", for: .normal)
+        button.backgroundColor = .clear
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+
+        button.layer.masksToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
+        countLabel.text = "Количество: \(Int(stepper.value))"
         navigationController?.navigationBar.isHidden = false
         view.backgroundColor = .white
     
@@ -64,26 +116,48 @@ class CoffeeDetailViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
+    @objc func stepperValueChanged (_ sender: UIStepper!) {
+        
+        countLabel.text = "Количество: \(Int(sender.value))"
+        print(sender.value)
+
+    }
+    
     func setupViews () {
         view.addSubview(coffeeImage)
         view.addSubview(titleLabel)
         view.addSubview(descriptionLabel)
+        view.addSubview(countLabel)
         view.addSubview(priceLabel)
-        
-        
+        view.addSubview(sizeLabel)
+        view.addSubview(stepper)
+        view.addSubview(segmentControl)
+        view.addSubview(addToOrderButton)
         
         let layots: [NSLayoutConstraint] = [
             coffeeImage.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20),
             coffeeImage.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20),
             coffeeImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            coffeeImage.heightAnchor.constraint(equalToConstant: view.frame.height / 2),
+            coffeeImage.heightAnchor.constraint(equalToConstant: view.frame.height / 3),
             titleLabel.topAnchor.constraint(equalTo: coffeeImage.bottomAnchor, constant: 20),
             titleLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20),
             descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
             descriptionLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20),
             descriptionLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20),
+            countLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20),
+            countLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 20),
+            stepper.leftAnchor.constraint(equalTo: descriptionLabel.leftAnchor),
+            stepper.topAnchor.constraint(equalTo: countLabel.topAnchor, constant: 30),
+            sizeLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20),
+            sizeLabel.topAnchor.constraint(equalTo: stepper.bottomAnchor, constant: 20),
+            segmentControl.topAnchor.constraint(equalTo: sizeLabel.bottomAnchor, constant: 10),
+            segmentControl.leftAnchor.constraint(equalTo: sizeLabel.leftAnchor),
             priceLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            priceLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10),
+            priceLabel.topAnchor.constraint(equalTo: segmentControl.bottomAnchor, constant: 10),
+            addToOrderButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            addToOrderButton.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: 20),
+            addToOrderButton.widthAnchor.constraint(equalToConstant: 200),
+            addToOrderButton.heightAnchor.constraint(equalToConstant: 42)
         ]
         
         NSLayoutConstraint.activate(layots)
